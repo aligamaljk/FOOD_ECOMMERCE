633c26dd3f7e8480ad1ffbfbcee1158eb49ccb53
Ali Gamal <ali.gamal2282003@gmail.com>
2024-05-21 09:55:00 +0300

    add million

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import './index.scss'
 import 'swiper/css';
 import { store } from './store/Store.tsx';
 import { Provider } from 'react-redux';
+import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
